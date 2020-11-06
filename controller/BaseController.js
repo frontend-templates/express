cfeed4e01e4fe9f49f3485b515b6367169ec7691
yanxianping <yanxianping@haodf.com>
2020-11-06 12:17:07 +0800

    feat: create error page, add resErrorPage and resErrorJson function for BaseController

--- controller/BaseController.js
+++ controller/BaseController.js
@@ -15,9 +15,34 @@ class BaseController {
         this.next = next;
     }
 
-    // TODO 校验是否登录
-    // TODO 渲染错误页面
-    // TODO 响应错误json
+    /**-----------------------------------------------
+     *                   渲染错误
+     *----------------------------------------------*/
+    /**
+     * 渲染错误页面
+     * @param code <number> 需要渲染的错误码
+     */
+    resErrorPage(code, data = {errorCode: '', data: {}, msg: ''}) {
+        switch (code) {
+            case /^4/:
+                this.res.render('error/4--', data);
+                break;
+            case /^5/:
+                this.res.render('error/5--', data);
+                break;
+            default :
+                this.res.render('error/default', data);
+                break;
+        }
+    }
+
+    /**
+     * 渲染错误ajax请求
+     * @param data <Object> 需要响应的json
+     */
+    resErrorJson(data = {errorCode: '', data: {}, msg: ''}) {
+        this.res.json(data);
+    }
 }
 
 module.exports = BaseController;
---
+++ views/error/default.hb
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="zh-CN">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no">
+    <title>出错啦~</title>
+</head>
+<style type="text/css">
+    * {
+        margin: 0;
+        padding: 0;
+    }
+
+    html, body {
+        width: 100%;
+        height: 100%;
+        overflow: hidden;
+    }
+
+</style>
+<body>
+<div>
+    this is default error page
+    tip: the ui is building
+</div>
+</body>
+<script type="application/javascript"></script>
+</html>
